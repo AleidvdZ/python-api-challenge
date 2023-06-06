56c6112775529a72cd754c35ee852d9ccd09b1ef
AleidvdZ <131220504+AleidvdZ@users.noreply.github.com>
2023-06-06 14:15:38 -0500

    0606 work through csv

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "c7bc1b7c1c6a326f1d03f1e051207640"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "572e56af741d432d8b0d13bb04ba3442"
